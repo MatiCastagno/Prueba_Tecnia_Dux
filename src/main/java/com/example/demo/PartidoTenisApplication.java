83e17e3f34c1324d112d816cb76c14e740fb9f26
MatiCastagno <matiicastagno007@gmail.com>
2022-07-13 23:59:47 -0300

    first commit

---
+++ src/main/java/com/example/demo/PartidoTenisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidoTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartidoTenisApplication.class, args);
+	}
+
+}
